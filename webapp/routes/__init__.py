bde82b8e74788589c55f44f9010b0b8bd4c2df1a
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2022-02-08 13:22:19 -0500

    a GET request was added for debugging

--- webapp/routes/__init__.py
+++ webapp/routes/__init__.py
@@ -19,42 +19,45 @@ def init(app):
             "users": users
         })
 
-    @app.route('/loadfiles', methods=['POST'])
+    @app.route('/loadfiles', methods=['POST', 'GET'])
     def load_files():
-        data = request.json
-        try:
-            # Create .cfg & .dat files
-            user = data["user"]
-            cfg_filename = f"{user}_test.cfg"
-            dat_filename = f"{user}_test.dat"
-            cfg = CustomFile(cfg_filename)
-            dat = CustomFile(dat_filename)
-            # .cfg
-            if not cfg.exists():
-                cfg.create()
-            cfg.write(data["cfg"], overwrite=True)
-            # .dat
-            if not dat.exists():
-                dat.create()
-            dat.write(data["dat"], overwrite=True)
+        if request.method == "POST":
+            data = request.json
+            try:
+                # Create .cfg & .dat files
+                user = data["user"]
+                cfg_filename = f"{user}_test.cfg"
+                dat_filename = f"{user}_test.dat"
+                cfg = CustomFile(cfg_filename)
+                dat = CustomFile(dat_filename)
+                # .cfg
+                if not cfg.exists():
+                    cfg.create()
+                cfg.write(data["cfg"], overwrite=True)
+                # .dat
+                if not dat.exists():
+                    dat.create()
+                dat.write(data["dat"], overwrite=True)
 
-            # Process files
-            # .cfg
-            print("CFG: ", cfg.read())
-            #.dat
-            print("DAT: ", dat.read())
+                # Process files
+                # .cfg
+                print("CFG: ", cfg.read())
+                #.dat
+                print("DAT: ", dat.read())
 
-            # Delete files
-            cfg.remove()
-            dat.remove()
+                # Delete files
+                cfg.remove()
+                dat.remove()
 
-            return Response(jsonify({
-                "status": "ok",
-                "message": "files created, processed and deleted"
-            }).data, status=200, mimetype="application/json")
-            
-        except:
-            return Response(jsonify({
-                "status": "Error while creating creating/processing/removing files"
-            }).data, status=500)
+                return Response(jsonify({
+                    "status": "ok",
+                    "message": "files created, processed and deleted"
+                }).data, status=200, mimetype="application/json")
+                
+            except:
+                return Response(jsonify({
+                    "status": "Error while creating creating/processing/removing files"
+                }).data, status=500)
+        elif request.method == "GET":
+            return "Try a POST request"
 
